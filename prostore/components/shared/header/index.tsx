186d3948da7e605038bbb0427a6e6cc17dd2052a
Andre Silva <andrenations@gmail.com>
2025-02-07 18:55:45 -0500

    Added useIsClient hook to ensure styles are applied only after component mount. Integrated Menu component for navigation. Updated Header to prevent rendering until mounted, improving hydration consistency.

--- prostore/components/shared/header/index.tsx
+++ prostore/components/shared/header/index.tsx
@@ -1,12 +1,28 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
+"use client"
+import { useIsClient } from "@/hooks/useIsClient";
+
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import Menu from "./menu";
+
+
 
 const Header = () => {
+
+// Ensure styles are applied only after the component mounts
+
+const mounted = useIsClient(); // Using the custom hook
+
+if (!mounted) {
+  return null; // Return nothing until the component has mounted
+}
+
+
+
   return (
     <header className="w-full border-b">
+       
       <div className="wrapper flex-between">
         <div className="flex-start">
           <Link href="/" className="flex-start">
@@ -23,18 +39,8 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <Button asChild variant="ghost">
-            <Link href="/">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild variant="ghost">
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+
+        <Menu />
       </div>
     </header>
   );

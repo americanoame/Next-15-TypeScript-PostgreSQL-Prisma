2b20f1e90dbf83367d229617484e83d2bc3068c0
Andre Silva <andrenations@gmail.com>
2025-02-08 20:54:48 -0500

    change the style,  width and delete the image

--- prostore/components/shared/header/index.tsx
+++ prostore/components/shared/header/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useIsClient } from "@/hooks/useIsClient";
 
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
@@ -21,22 +21,22 @@ if (!mounted) {
 
 
   return (
-    <header className="w-full border-b">
+    <header className="w-full ">
        
       <div className="wrapper flex-between">
         <div className="flex-start">
           <Link href="/" className="flex-start">
-            <Image
+            {/* <Image
               src="/images/logo.svg"
               alt={` ${APP_NAME}  logo`}
               height={48}
               width={48}
               priority={true}
-            />
+            /> */}
 
-            <span className="span hidden lg:block font-size text2xl ml-3">
+            {/* <span className="span hidden lg:block font-size text2xl ml-3">
               {APP_NAME}
-            </span>
+            </span> */}
           </Link>
         </div>
 

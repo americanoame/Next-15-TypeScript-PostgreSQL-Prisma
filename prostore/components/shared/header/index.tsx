c9dc06947b9342291bc69d6aa48592c4feeedb87
Andre Silva <andrenations@gmail.com>
2025-02-10 23:13:04 -0500

    refactor the code

--- prostore/components/shared/header/index.tsx
+++ prostore/components/shared/header/index.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+
 import { useIsClient } from "@/hooks/useIsClient";
 
 import Image from "next/image";
@@ -6,40 +7,40 @@ import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
 
-
+import CategoriesDrawer from "./categories-drawer";
+import Search from "./search";
 
 const Header = () => {
+  // Using the custom hook to check if the component is mounted on the client side
+  const mounted = useIsClient();
 
-// Ensure styles are applied only after the component mounts
-
-const mounted = useIsClient(); // Using the custom hook
-
-if (!mounted) {
-  return null; // Return nothing until the component has mounted
-}
-
-
+  // Return null (render nothing) until the component has mounted on the client
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <header className="w-full  ">
-       
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <CategoriesDrawer />
+          <Link href="/" className="flex-start ml-4">
             <Image
               src="/images/logo.svg"
               alt={` ${APP_NAME}  logo`}
               height={48}
               width={48}
               priority={true}
-            /> 
+            />
 
-             <span className="span hidden lg:block font-size text2xl ml-3">
+            <span className="span hidden lg:block font-size text2xl ml-3">
               {APP_NAME}
             </span>
           </Link>
         </div>
-
+        <div className="ml-28 hidden md:block">
+          <Search />
+        </div>
         <Menu />
       </div>
     </header>

0fb58bee808a2e02d7d5168e5caae475cb54d7c2
Andre Silva <andrenations@gmail.com>
2025-02-10 23:27:16 -0500

    refactor the code

--- prostore/lib/actions/product.actions.ts
+++ prostore/lib/actions/product.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 // import { PrismaClient } from '@prisma/client';
-import {prisma} from "@/db/prisma"
+import {prisma} from "@/components/db/prisma"
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
@@ -13,6 +13,8 @@ export async function getLatestProducts() {
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
   });
+
+  // console.log(data);
 
 //   return data;
 
@@ -28,4 +30,53 @@ export async function getProductBySlug(slug: string) {
     where: { slug: slug },
   });
 }
+
+
+
+export async function getAllCategories() {
+  const data = await prisma.product.groupBy({
+    by: ['category'],
+    _count: true,
+  });
+
+  return convertToPlainObject(data);
+}
+
+
+
+
+// Get product categories
+// export async function getAllCategories() {
+//   const data = await prisma.product.groupBy({
+//     by: ['category'],
+//     _count: true,
+//   });
+
+//   // Convert each item to a plain object, ensuring no special Prisma properties are passed
+//   const plainData = data.map(item => {
+//     // Manually construct a plain object
+//     return {
+//       category: item.category,
+//       count: item._count ? item._count : undefined,
+//     };
+//   });
+
+//   return plainData;
+// }
+
+
+// Fetch recent products
+export async function getRecentProducts() {
+  const data = await prisma.product.findMany({
+    where: {
+      isRecent: true, // Only products that are recent
+    },
+    take: LATEST_PRODUCTS_LIMIT, // Limit the number of products fetched
+    orderBy: { createdAt: 'desc' },
+  });
+
+  return convertToPlainObject(data);
+}
+
+
 
